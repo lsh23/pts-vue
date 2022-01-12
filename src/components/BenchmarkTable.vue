acd9c44b12ef11e34c01324c0ed89bdd1bafb8b9
lsh23 <sehyeong.lee@nhn.com>
2022-01-12 11:23:29 +0900

    add BenchmarkTable Component

---
+++ src/components/BenchmarkTable.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-container>
+  <v-data-table
+    :headers="headers"
+    :items="benchmarks"
+    :items-per-page="5"
+    class="elevation-1"
+  ></v-data-table>
+  </v-container>
+</template>
+
+<script>
+  // import {mapGetters} from 'vuex'
+  export default {
+    name: 'BenchmarkTable',
+    data: () => ({
+              headers: [
+          {
+            text: 'uuid',
+            align: 'start',
+            sortable: false,
+            value: 'uuid',
+          },
+          { text: 'CSP', value: 'csp' },
+          { text: 'FlAVOR', value: 'flavor' },
+          { text: 'TEST_SUITE', value: 'test_suite' },
+          { text: 'DATE', value: 'date' },
+          { text: 'STATUS', value: 'status' },
+        ],
+        // benchmarks: [
+        //   {
+        //       "uuid": "9J3XBT7n90N7",
+        //       "csp": "NHN",
+        //       "flavor": "m2.c2m4",
+        //       "test_suite": "tmp-test",
+        //       "date": "2022-01-10T19:00:48.782475+09:00",
+        //       "start_datetime": null,
+        //       "end_datetime": null,
+        //       "status": "INSTANCE_CREATED"
+        //   },
+        //   {
+        //       "uuid": "uYULfJgCVMXy",
+        //       "csp": "NHN",
+        //       "flavor": "m2.c2m4",
+        //       "test_suite": "tmp-test",
+        //       "date": "2022-01-10T16:01:11.208235+09:00",
+        //       "start_datetime": null,
+        //       "end_datetime": null,
+        //       "status": "INSTANCE_CREATED"
+        //   }
+        // ]
+    }),
+    computed:{
+      benchmarks(){
+        // console.log(this.$store.getters.getBenchmarks);
+        return this.$store.getters.getBenchmarks;
+
+      }
+      // ...mapGetters(['getBenchmarks'])
+    },
+    created(){
+        this.$store.dispatch('fetchBenchmarks')
+    }
+  }
+</script>
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,99 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-
+    state:{
+        benchmarks:[],
+        results:[],
+        graphs:[]
+    },
+    getters:{
+        getBenchmarks(state){
+            return state.benchmarks
+        },
+        getResults(state){
+            return state.results
+        },
+        getGraphs(state){
+            return state.graphs
+        }
+    },
+    mutations:{
+        setBenchmarks(state, benchmarks){
+           state.benchmarks = benchmarks
+        },
+        setResults(state, results){
+            state.results = results
+        },
+        setGraphs(state, graphs){
+            state.graphs = graphs 
+        }        
+    },
+    actions:{
+        fetchBenchmarks(context){
+            axios.get('http://133.186.134.206:18000/v1/pts/benchmarks/')
+                .then(res=>context.commit('setBenchmarks',res.data))
+        },
+        fetchResults(context){
+            axios.get('http://133.186.134.206:18000/v1/pts/results/')
+                .then(res=>context.commit('setResults',res.data))
+        },
+        fetchGraphs(context){
+            axios.get('http://133.186.134.206:18000/v1/pts/graphs/')
+                .then(res=>context.commit('setGraphs',res.data))            
+        }
+    }
 });
+
+
+
+
+// vuex cheat sheet
+// state:{
+//     message: "hello vue.js"
+// }
+
+// {{ this.$store.state.message }}
+
+// getters: {
+//     getNumber(state){
+//         return state.num
+//     },
+//     doubleNumber(state){
+//         return state.num * 2
+//     }
+// }
+
+// {{ this.$store.getters.getNumber }}
+
+// mutations: {
+//     printNumbers(state){
+//         return state.num
+//     },
+//     sumNumbers(state, anotherNum){
+//         return state.num + anotherNum;
+//     },
+//     modifyState(state, payload){
+//         console.log(payload, str);
+//         return state.storeNum += payload.num;
+//     }
+// }
+
+// this.$store.commit('printNumbers');
+// this.$store.commit('sumNumbers',20);
+// this.$store.commit('modifyState',{
+//     str: 'passed from payload',
+//     num: 20
+// })
+
+// actions:{
+//     fetchProductData(context){
+//         return axios.get('')
+//             .then(res=>context.commit('setData',res))
+//     }
+// }
+
+// this.$store.dispatch('fetchProductData')
